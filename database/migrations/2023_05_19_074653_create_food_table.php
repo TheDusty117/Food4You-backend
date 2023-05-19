eb70778214ba0bcea5de3a57b7a28563227eb352
TheDusty117 <gabrieledipassio@gmail.com>
2023-05-19 10:14:49 +0200

    FEAT migration Food done

---
+++ database/migrations/2023_05_19_074653_create_food_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 100);
+            $table->decimal('price', 4,2);
+            $table->string('ingredients', 150);
+            $table->text('description');
+            $table->tinyInteger('vegan')->default(0);
+            $table->tinyInteger('spicy')->default(0);
+            $table->tinyInteger('availability')->default(1);
+            $table->tinyInteger('visibility')->default(1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food');
+    }
+};
