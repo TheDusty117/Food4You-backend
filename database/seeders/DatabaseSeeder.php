2b35f4d0543a1be07137ec01d1538ca39445da22
TheDusty117 <gabrieledipassio@gmail.com>
2023-05-21 10:57:35 +0200

    FIX orderseeder in dbseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
             RestaurantSeeder::class,
             CategorySeeder::class,
             FoodSeeder::class,
+            OrderSeeder::class,
 
         ]);
     }
