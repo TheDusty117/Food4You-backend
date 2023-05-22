b36b34e7161a05bdbc67cde9d8600c95b92caab7
TheDusty117 <gabrieledipassio@gmail.com>
2023-05-22 14:23:29 +0200

    FEAT creating index stuff

--- resources/views/foods/index.blade.php
+++ resources/views/foods/index.blade.php
@@ -1,7 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <h1>Food index</h1>
+    <div class="container">
+        <table class="table table-striped table-inverse table-responsive">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Slug</th>
+                    <th>Availability</th>
+                    <th>Visibility</th>
+                    <th>Created at</th>
+                    <th>Updated at</th>
+
+                    {{-- thead di bottoni --}}
+                    <th>Modifica</th>
+                    <th>Cancella</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($foods as $food)
+                    <tr>
+                        <td>{{ $food->id }}</td>
+                        <td>{{ $food->name }}</td>
+                        <td>{{ $food->price }}</td>
+                        <td>{{ $food->slug }}</td>
+                        <td>{{ $food->availability }}</td>
+                        <td>{{ $food->visibility }}</td>
+                        <td>{{ $food->created_at }}</td>
+                        <td>{{ $food->updated_at }}</td>
+
+                        {{-- BOTTONI DI MODIFICA E CANCELLAZIONE --}}
+                        <td>
+                            <button class="btn btn-secondary">
+                                M
+                            </button>
+                        </td>
+                        <td>
+                            <button class="btn btn-danger">
+                                X
+                            </button>
+                        </td>
+
+                    </tr>
+                @empty
+                    <tr>
+                        <th colspan="6">Nessun Food trovato</th>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
     </div>
 @endsection
