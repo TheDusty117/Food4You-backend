534406dca9072233b7c1c1f08414433b1846e0e8
TheDusty117 <gabrieledipassio@gmail.com>
2023-05-22 11:35:02 +0200

    FEAT slug created correctly

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+// use Illuminate\Database\Eloquent\HasFactory;
 
 class Food extends Model
 {
 
-    protected $filable = ['name','price','ingredients','description','vegan','spicy','availability','visibility'];
+    protected $filable = ['name','price','ingredients','description','vegan','spicy','availability','visibility','slug'];
 
 
     public function restaurant()
@@ -17,6 +18,7 @@ class Food extends Model
     }
 
 
-    use HasFactory, SoftDeletes;
+    // use HasFactory;
+    use SoftDeletes;
 
 }
