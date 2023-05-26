7ed69080af4c978ce717ea25a2937ae9496fed6c
nicola00 <nicolajobs00@gmail.com>
2023-05-26 16:38:57 +0200

    FEAT change seeder user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 
 /**
@@ -18,14 +19,6 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'telephone_number' => random_int(1000000000, 9999999999),
-            'remember_token' => Str::random(10),
-        ];
     }
 
     /**
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Restaurant;
 use Illuminate\Support\Facades\Hash;
-use Faker\Generator as Faker;
+use Faker\Factory as Faker;
+use Illuminate\Support\Str;
+
 
 
 class UserSeeder extends Seeder
@@ -18,151 +20,37 @@ class UserSeeder extends Seeder
      * @return void
      */
 
-    public function run(Faker $faker)
+    public function run()
     {
-        \App\Models\User::factory(20)->create();
-
-        $users = [
-            [
-                'name' => 'La Cuccuma',
-                'telephone_number' => '06 7720 1361',
-                'email' => 'lacuccuma@info.it',
-
-            ],
-            [
-                'name' => 'Trieste Pizza Roma',
-                'telephone_number' => '085 421 40 38',
-                'email' => 'info@trieste.pizza',
+        $faker = Faker::create('it_IT');
+        $collection = collect(['@gmail.com', '@libero.it', '@info.food4you', '@outlook.it', '@hotmail.com']);
 
 
-            ],
-            [
-                'name' => 'Pizzeria della Madonna dei Monti',
-                'telephone_number' => '06 6941 3112',
-                'email' => 'madonnadeimonti@info.it',
+
+        for ($i = 0; $i < 50; $i++) {
+            $users = new User();
+            $users->name = $faker->name();
+            $users->email = Str::slug($users->name, '') . $collection->random();
+            $users->telephone_number = $faker->phoneNumber;
+            $users->telephone_number = str_replace(' ', '', $users->telephone_number); // Rimuovi spazi
+            $users->telephone_number = str_replace('-', '', $users->telephone_number); // Rimuovi trattini
+            $users->telephone_number = str_replace('/', '', $users->telephone_number); // Rimuovi slash
+            $users->telephone_number = str_replace('.', '', $users->telephone_number); // Rimuovi punti
+            $users->telephone_number = '+39' . substr($users->telephone_number, -10); // Aggiungi prefisso italiano
 
 
-            ],
-            [
-                'name' => 'La Prezzemolina',
-                'telephone_number' => '06 9484 3773',
-                'email' => 'laprezzemolina@tiscali.it',
+            $users->email_verified_at = now();
+            $users->password = Hash::make($users->name . $faker->randomNumber(2));
+            $users->remember_token = Str::random(10);
 
-            ],
-            [
-                'name' => 'Piccolo Buco',
-                'telephone_number' => '06 6938 0163',
-                'email' => 'piccolobuco@gmail.com',
-
-            ],
-            [
-                'name' => 'Sushisen',
-                'telephone_number' => '06-57.56.945',
-                'email' => 'INFO@SUSHISEN.IT',
-
-            ],
-            [
-                'name' => 'Tora Sushi Ristorante Giapponese e Cucina Asiatica',
-                'telephone_number' => '06 8913 4334',
-                'email' => 'torasushi@gmail.com',
-
-            ],
-            [
-                'name' => 'Shiroya - ristorante giapponese',
-                'telephone_number' => '06 6476 0753',
-                'email' => 'info@shiroya.it',
-
-            ],
-            [
-                'name' => 'Domò Sushi',
-                'telephone_number' => '366 457 6219',
-                'email' => 'domopariolisrl@gmail.com',
-
-            ],
-            [
-                'name' => 'Jaipur - Ristorante Indiano',
-                'telephone_number' => '06 580 3992',
-                'email' => 'infojaipur.@gmail.com',
-
-            ],
-            [
-                'name' => 'Himalaya Palace',
-                'telephone_number' => '06 582 6001',
-                'email' => 'info@himalayapalace.com',
-
-            ],
-            [
-                'name' => 'Bibliothè',
-                'telephone_number' => '06 678 1427',
-                'email' => 'info@bibliothe.guru',
-
-            ],
-            [
-                'name' => 'La Cucaracha',
-                'telephone_number' => '06 3974 6373',
-                'email' => 'info@lacucaracha.it',
-
-            ],
-            [
-                'name' => 'El Jalapeño',
-                'telephone_number' => '06 8913 5970',
-                'email' => 'info@eljalapeno.it',
-
-            ],
-            [
-                'name' => 'Casa Sánchez',
-                'telephone_number' => '06 4554 2607',
-                'email' => 'casasanchezroma@gmail.com',
-
-            ],
-            [
-                'address' => 'Vicolo della Renella, 94',
-                'telephone_number' => '06 4424 2807',
-                'email' => 'picosofficial@gmail.com',
-
-            ],
-            [
-                'name' => 'Ma Va ? Restaurant Roma',
-                'telephone_number' => '06 372 9134',
-                'email' => 'ristomava@libero.it',
-
-            ],
-            [
-                'name' => 'Nativa Ristorante',
-                'telephone_number' => '06 6400 8399',
-                'email' => 'info@leggimenu.it',
-
-            ],
-            [
-                'name' => 'O Famo Strano',
-                'telephone_number' => '327 004 0637',
-                'email' => 'ofamostrano@libero.it',
-
-            ],
-            [
-                'name' => 'By Luk',
-                'telephone_number' => '351 718 5863',
-                'email' => 'byluk@info.it',
-
-            ],
-        ];
-
-
-        // foreach ($users as $user) {
-        //     $new_user = new User();
-        //     $new_user->name = $user['name'];
-        //     $new_user->email = $user['email'];
-        //     $new_user->password = Hash::make($faker->word());
-        //     $new_user->telephone_number = $user['telephone_number'];
-
-        //     $new_user->save();
-        // }
-
-
+            $users->save();
+            // $users = new User();
+            // $users->name => fake()->name(),
+            // $users->email => fake()->safeEmail(),
+            // $users->email_verified_at => now(),
+            // $users->password => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            // $users->telephone_number => random_int(1000000000, 9999999999),
+            // $users->remember_token => Str::random(10),
+        }
     }
-
 };
-
-
-
-
