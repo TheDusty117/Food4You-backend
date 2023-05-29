e1daa5265fedcdef506f56efafa2e6e874e4acf5
TheDusty117 <gabrieledipassio@gmail.com>
2023-05-29 12:12:46 +0200

    FEAT categories.restaurants

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -9,7 +9,7 @@ use App\Models\Restaurant;
 class RestaurantController extends Controller
 {
     public function index(){
-        $restaurants = Restaurant::with('categories')->paginate(5);
+        $restaurants = Restaurant::with('categories', 'categories.restaurants')->paginate(5);
 
         return response()->json([
             'success' => true,
