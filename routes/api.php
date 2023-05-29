34b6acde755058383fb2020bf36844964dc8c437
Blaser993 <corradodepinto.design@gmail.com>
2023-05-29 15:28:01 +0200

    nuova rotta

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('/restaurants',[RestaurantController::class,'index']);
+
+Route::get('/restaurants/{slug}', [PostController::class, 'show']);
